adc65e2e1e208da96b940b7672e8f723daa31200
NickBurrows <nick.github1@gmail.com>
2024-04-05 03:57:49 +0800

    新增Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen w-full dark:bg-gray-800 dark:text-white">
+      <h1 className="text-3xl text-blue-400 dark:text-red-400">Hello World!</h1>
     </div>
   );
 }
